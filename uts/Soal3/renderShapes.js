e605b874edde7ee076ea6aecb93744ac16e1e828
hanifaudah <hanifarkanaudah@gmail.com>
2022-04-07 11:46:50 +0700

    render multiple spheres

--- uts/Soal3/renderShapes.js
+++ uts/Soal3/renderShapes.js
@@ -1,7 +1,7 @@
 import { tetrahedron } from "./utils.js";
 
 function renderMainSPhere(props) {
-  const { gl, program } = props;
+  const { gl, program, left, right, bottom, top } = props;
   var numTimesToSubdivide = 3;
 
   var near = -10;
@@ -23,11 +23,6 @@ function renderMainSPhere(props) {
 
   var at = vec3(0.0, 0.0, 0.0);
   var up = vec3(0.0, 1.0, 0.0);
-
-  var left = -3.0;
-  var right = 3.0;
-  var top = 3.0;
-  var bottom = -3.0;
 
   var lightPosition = vec4(1.0, 1.0, 1.0, 0.0);
   var lightAmbient = vec4(0.2, 0.2, 0.2, 1.0);
@@ -141,13 +136,14 @@ function render(props) {
     bottom,
     top,
   } = props;
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
   eye = vec3(
     radius * Math.sin(theta) * Math.cos(phi),
     radius * Math.sin(theta) * Math.sin(phi),
     radius * Math.cos(theta)
   );
+
+  props.theta += 0.01;
 
   modelViewMatrix = lookAt(eye, at, up);
   projectionMatrix = ortho(left, right, bottom, top, near, far);
@@ -164,7 +160,23 @@ function render(props) {
 }
 
 function renderShapes(props) {
-  renderMainSPhere(props);
+  const { gl } = props;
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  renderMainSPhere({
+    ...props,
+    left: -3.0,
+    right: 3.0,
+    top: 3.0,
+    bottom: -3.0,
+  });
+  renderMainSPhere({
+    ...props,
+    left: -5.0,
+    right: 1.0,
+    top: 3.0,
+    bottom: -3.0,
+  });
 }
 
 export default renderShapes;
