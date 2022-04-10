2c97013fd9d348757b28d414c7cc0d581f0ee2f8
mushaffa huda <mushaffahuda01@gmail.com>
2022-04-10 22:59:09 +0700

    adding notes and edits for soal 2

--- uts/Soal2/index.js
+++ uts/Soal2/index.js
@@ -1,13 +1,23 @@
 "use strict";
 
 var gl;
-var positions = []
-var numPositions = 500;
+var canvas;
+
+var positions = [];
+
+var positionLoc;
+var xAwal;
+var xAkhir;
+var yAwal;
+var yAkhir;
+
+var CPValue;
 
 init();
 
+// init function
 function init() {
-    var canvas = document.getElementById("gl-canvas");
+    canvas = document.getElementById("gl-canvas");
 
     gl = canvas.getContext('webgl2');
     if (!gl) alert("WebGL 2.0 isn't available");
@@ -22,35 +32,89 @@ function init() {
     var program = initShaders(gl, "vertex-shader", "fragment-shader");
     gl.useProgram(program);
 
+    // Associate out shader variables with our data buffer
+    positionLoc = gl.getAttribLocation(program, "aPosition");
+
+    // function to print x and y value using midpoint algorithm
     document.getElementById("coordinateForm").onsubmit = function (e) {
         e.preventDefault();
-
         clear();
-
-        var xAwal = document.getElementById("x0").value;
-        var yAwal = document.getElementById("y0").value;
-
-        var xAkhir = document.getElementById("x1").value;
-        var yAkhir = document.getElementById("y1").value;
-
+        getValues();
         midPoint(xAwal, yAwal, xAkhir, yAkhir);
-
-        // Load the data into the GPU
-        var vBuffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, flatten(positions), gl.STATIC_DRAW);
-
-        // Associate out shader variables with our data buffer
-        var positionLoc = gl.getAttribLocation(program, "aPosition");
-        gl.vertexAttribPointer(positionLoc, 2, gl.FLOAT, false, 0, 0);
-        gl.enableVertexAttribArray(positionLoc);
-
+        initBuffer();
         render();
     }
+
 };
 
+// function for buffer init
+function initBuffer() {
+    // Load the data into the GPU
+    var vBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(positions), gl.STATIC_DRAW);
+
+    gl.vertexAttribPointer(positionLoc, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(positionLoc);
+};
+
+// function to get the start and end values of x and y
+function getValues() {
+    xAwal = document.getElementById("x0").value;
+    yAwal = document.getElementById("y0").value;
+    xAkhir = document.getElementById("x1").value;
+    yAkhir = document.getElementById("y1").value;
+}
+
+// function for drawing cardinal points (mata angin) to the canvas
+function printCP() {
+    CPValue = document.getElementById("CP").value
+    clear();
+    drawCP(CPValue);
+    initBuffer();
+    render();
+}
+
+// helper function for drawing cardinal points
+function drawCP(CP) {
+    if (CP == "N") {
+        midPoint(0, 0, 0, 10);
+    } else if (CP == "E") {
+        midPoint(0, 0, 10, 0);
+    } else if (CP == "S") {
+        midPoint(0, 0, 0, -10);
+    } else if (CP == "W") {
+        midPoint(0, 0, -10, 0);
+    } else if (CP == "NE") {
+        midPoint(0, 0, 10, 10);
+    } else if (CP == "SE") {
+        midPoint(0, 0, 10, -10);
+    } else if (CP == "SW") {
+        midPoint(0, 0, -10, -10);
+    } else if (CP == "NW") {
+        midPoint(0, 0, -10, 10);
+    }
+}
+
+// clear function
+function clear() {
+    positions = [];
+    document.getElementById('output').innerHTML = "";
+    render();
+}
+
+// render function
+function render() {
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.drawArrays(gl.LINE_STRIP, 0, positions.length);
+}
+
+// midpoint function 
+// taken and modified from source : https://www.geeksforgeeks.org/mid-point-line-generation-algorithm/
+// modified to accomodate all slope ( 4 quadrants )
 function midPoint(X1, Y1, X2, Y2) {
 
+    // source : https://stackoverflow.com/questions/64830053/javascript-midpoint-line-drawing-algorithm
     let xReflect = 1;
     if (X2 < X1) {
         X1 = -X1;
@@ -69,7 +133,6 @@ function midPoint(X1, Y1, X2, Y2) {
     let dx = X2 - X1;
     let dy = Y2 - Y1;
 
-    // iterate through value of X
     if (X1 !== X2) {
 
         // initial value of decision
@@ -78,29 +141,23 @@ function midPoint(X1, Y1, X2, Y2) {
         let x = X1, y = Y1;
 
         // Plot initial given point
-        console.log(x * xReflect + "," + y * yReflect)
         document.getElementById('output').innerHTML += x * xReflect + ", " + y * yReflect + "<br/>";
-        positions.push(vec2((x * xReflect) / 10, (y * yReflect) / 10))
+        positions.push(vec2((x * xReflect) / 10, (y * yReflect) / 10));
 
         while (x < X2) {
             x++;
 
-            // E or East is chosen
             if (d < 0)
                 d = d + dy;
 
-            // NE or North East is chosen
             else {
                 d += (dy - dx);
                 y++;
             }
 
             // Plot intermediate points
-            // putpixel(x,y) is used to print
-            // pixel of line in graphics
-            console.log(x * xReflect + "," + y * yReflect)
             document.getElementById('output').innerHTML += x * xReflect + ", " + y * yReflect + "<br/>";
-            positions.push(vec2((x * xReflect) / 10, (y * yReflect) / 10))
+            positions.push(vec2((x * xReflect) / 10, (y * yReflect) / 10));
         }
     } else {
 
@@ -110,38 +167,23 @@ function midPoint(X1, Y1, X2, Y2) {
         let x = X1, y = Y1;
 
         // Plot initial given point
-        console.log(x * xReflect + "," + y * yReflect)
         document.getElementById('output').innerHTML += x * xReflect + ", " + y * yReflect + "<br/>";
-        positions.push(vec2((x * xReflect) / 10, (y * yReflect) / 10))
+        positions.push(vec2((x * xReflect) / 10, (y * yReflect) / 10));
 
         while (y < Y2) {
             y++;
 
-            // N or North is chosen
             if (d < 0)
                 d = d + dx;
 
-            // NE or North East is chosen
             else {
                 d += (dx - dy);
                 x++;
             }
 
             // Plot intermediate points
-            console.log(x * xReflect + "," + y * yReflect)
             document.getElementById('output').innerHTML += x * xReflect + ", " + y * yReflect + "<br/>";
-            positions.push(vec2((x * xReflect) / 10, (y * yReflect) / 10))
+            positions.push(vec2((x * xReflect) / 10, (y * yReflect) / 10));
         }
     }
-}
-
-function clear() {
-    positions = []
-    document.getElementById('output').innerHTML = "";
-    render();
-}
-
-function render() {
-    gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.drawArrays(gl.LINE_STRIP, 0, positions.length);
 }
--- uts/readme.md
+++ uts/readme.md
@@ -2,6 +2,12 @@
 
 ## Gambaran umum tentang program yang dibuat
 
+### Soal 2
+
+Pada program ini algoritma midpointline dimodifikasi sehingga dapat mengakomodasi semua slope dengan melewati constraint awal dimana `x2 > x1` dan `y2 > y1` harus terpenuhi. maka input dapat diberi sembarang selama memenuhi batasan koordinat (`-10 < x, y < 10`).
+
+User akan memberi input sesuai kolom input yang ada dan hasil akhir dari garis akan tergambar pada canvas dan juga ditampilkannya tiap titik - titik yang dipilih oleh algoritma guna menggambar garis tersebut pada panel output.
+
 ### Soal 3
 
 Program ini menampilkan 3 benda 3D yang merepresentasikan molekul H2O. Terdapat 1 benda besar (oksigen)
@@ -30,6 +36,12 @@ UTS-Raster
 │
 └───Common
 │
+└───Soal2
+    │   index.html
+    │   index.css
+    │   index.js
+    │   alert.css
+│
 └───Soal3
     │   index.html
     │   index.css
@@ -47,6 +59,17 @@ Pada halaman ini terdapat tiga tombol, yang masing-masing mengarah pada Soal 1-3
 ### Navbar
 
 Setelah membuka Soal 1-3, terdapat navbar pada bagian atas halaman
+
+### Soal 2
+
+Pada halaman ini terdapat 1 canvas, 4 kolom input, 1 tombol **Draw Line**, 1 dropdown berisi 8 **Cardinal Points** (arah mata angin), dan output panel. berikut adalah penjabaran fungsi - fungsinya.
+
+1. 4 kolom input terdiri dari **xAwal**, **xAkhir**, **yAwal** dan **yAkhir**. tiap input menandakan koordinat awal dan akhir dari garis yang akan dibuat menggunakan algoritma midpointline. garis dapat terlihat setelah koordinat diinput dan tombol **Draw Line** ditekan.
+
+2. dropdown **Cardinal Points** atau arah mata angin dapat dipilih dan menggambar semua arah mata angin pada canvas.
+
+3. output panel mengeluarkan semua output dari titik - titik yang akan digambar menjadi garis
+
 
 ### Soal 3
 
@@ -68,3 +91,8 @@ Berikut posisi kamera setelah menekan tombol 1-4:
 - Menyusun struktur proyek
 - Membuat halaman utama
 - Membuat navbar
+
+(18061865272) Mushaffa Huda:
+
+- Mengerjakan Soal 2
+- Membuat Design CSS untuk proyek
