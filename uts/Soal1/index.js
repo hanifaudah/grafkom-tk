f7c1cb2e320ff407c8fb83d7491ade497dec67d9
hanifanggawi <hanifanggawi45@gmail.com>
2022-04-10 16:10:21 +0700

    refactor: setup for multiple shapes

--- uts/Soal1/index.js
+++ uts/Soal1/index.js
@@ -37,25 +37,19 @@ function getConst(key) {
   return {
     'shape' : SHAPES,
     'size' : SIZES,
-    'speed': SPEEDS
+    'speed': SPEEDS,
+    'spin' : SPIN
   }[key]
 }
 
 var state = {
   shape: SHAPES.Square,
-  speed: SPEEDS.Fast,
-  size: SIZES.Small,
+  speed: SPEEDS.Medium,
+  size: SIZES.Medium,
   spin: false
 }
 
-function setStateListeners() {
-  const options = document.querySelectorAll('select')
-  Array.from(options).forEach(option => {
-    option.addEventListener('change', () => {
-      setState(option.name, getConst(option.name)[option.value] )
-    })
-  })
-}
+
 
 function setState(key, value) {
   state[key] = value
@@ -64,47 +58,109 @@ function setState(key, value) {
 // Initiated and modified from https://github.com/idofilin/webgl-by-example/tree/master/raining-rectangles
 ;(function(){
     "use strict"
+
+    function setStateListeners() {
+      const options = document.querySelectorAll('select')
+      Array.from(options).forEach(option => {
+        option.addEventListener('change', () => {
+          setState(option.name, getConst(option.name)[option.value])
+          if (option.name === 'shape') {
+            setupAnimation('load')
+          }
+        })
+      })
+    }
+
     window.addEventListener("load", setupAnimation, false);
     window.addEventListener("load", setStateListeners, false);
 
     var gl,
       timer,
-      rainingRect,
       scoreDisplay,
+      shape,
       missesDisplay;
-   
+
     function setupAnimation (evt) {
       window.removeEventListener(evt.type, setupAnimation, false);
       if (!(gl = getRenderingContext()))
         return;
       gl.enable(gl.SCISSOR_TEST);
-    
-      rainingRect = new Rectangle();
-      timer = setTimeout(drawAnimation, 17);
+      
+      switch (state.shape) {
+        case SHAPES.Square:
+          shape = new Rectangle();
+          timer = setTimeout(drawSquare(), 17);
+        case SHAPES.Triangle:
+          // handle triangle
+        case SHAPES.Circle:
+          // handle circle
+      }
       document.querySelector("canvas").addEventListener("click", playerClick, false);
       var displays = document.querySelectorAll("strong");
       scoreDisplay = displays[0];  
       missesDisplay = displays[1]; 
     }
-    
+
     var score = 0,
       misses = 0;
-    function drawAnimation () {
-      gl.scissor(rainingRect.position[0], rainingRect.position[1], rainingRect.size[0] , rainingRect.size[1]);
+
+
+    function drawSquare () {
+      gl.scissor(shape.position[0], shape.position[1], shape.size[0] , shape.size[1]);
       gl.clear(gl.COLOR_BUFFER_BIT);
-      gl.clear(gl.COLOR_BUFFER_BIT);
-      rainingRect.position[1] -= rainingRect.velocity;
-      if (rainingRect.position[1] < 0) {
+      shape.position[1] -= shape.velocity;
+      if (shape.position[1] < 0) {
         misses += 1;
         missesDisplay.innerHTML = misses;
-        rainingRect = new Rectangle();
+        shape = new Rectangle();
       }
       // We are using setTimeout for animation.  So we reschedule
-      // the timeout to call  drawAnimation again in 17ms.
+      // the timeout to call  drawSquare again in 17ms.
       // Otherwise we won't get any animation.
-      timer = setTimeout(drawAnimation, 17);
+      timer = setTimeout(drawSquare, 17);
     }
-    
+
+    class Rectangle {
+      constructor() {
+        this.size = [5 + 120 * state.size, 5 + 120 * state.size];
+        this.position = [
+          Math.random() * (gl.drawingBufferWidth - this.size[0]),
+          gl.drawingBufferHeight,
+        ];
+        this.velocity = 1.0 + 6.0 * state.speed;
+        this.color = [0,0,0]
+        gl.clearColor(...selectRandom(shapeColors));
+      }
+
+      draw = () => {
+        gl.scissor(this.position[0], this.position[1], this.size[0] , this.size[1]);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+        this.position[1] -= this.velocity;
+        if (this.position[1] < 0) {
+          misses += 1;
+          missesDisplay.innerHTML = misses;
+          shape = new Rectangle();
+        }
+        // We are using setTimeout for animation.  So we reschedule
+        // the timeout to call  drawSquare again in 17ms.
+        // Otherwise we won't get any animation.
+        timer = setTimeout(this.draw, 17);
+      }
+    }
+
+    function isClickInsideShape(position) {
+      const diffPos = [
+        position[0] - shape.position[0],
+        position[1] - shape.position[1],
+      ];
+      return  (
+        diffPos[0] >= 0 &&
+        diffPos[0] < shape.size[0] &&
+        diffPos[1] >= 0 &&
+        diffPos[1] < shape.size[1]
+      )
+    }
+ 
     function playerClick (evt) {
       // We need to transform the position of the click event from
       // window coordinates to relative position inside the canvas.
@@ -115,50 +171,27 @@ function setState(key, value) {
           evt.pageX - evt.target.offsetLeft,
           gl.drawingBufferHeight - (evt.pageY - evt.target.offsetTop),
         ];
+
       // if the click falls inside the rectangle, we caught it.
       // Increment score and create a new rectangle.
-      var diffPos = [ position[0] - rainingRect.position[0],
-          position[1] - rainingRect.position[1] ];
-      if ( diffPos[0] >= 0 && diffPos[0] < rainingRect.size[0]
-          && diffPos[1] >= 0 && diffPos[1] < rainingRect.size[1] ) {
+      if (isClickInsideShape(position)) {
         score += 1;
         scoreDisplay.innerHTML = score;
-        rainingRect = new Rectangle();
+        shape = new Rectangle();
       }
     }
-    
-    function Rectangle () {
-      // Keeping a reference to the new Rectangle object, rather
-      // than using the confusing this keyword.
-      var rect = this;
-      // We get three random numbers and use them for new rectangle
-      // size and position. For each we use a different number,
-      // because we want horizontal size, vertical size and
-      // position to be determined independently.
-      var randNums = getRandomVector();
-      rect.size = [ 5 + 120 * state.size, 5 + 120 * state.size ];  
-      rect.position = [
-        randNums[2]*(gl.drawingBufferWidth - rect.size[0]),
-        gl.drawingBufferHeight
-      ];
-      rect.velocity = 1.0 + 6.0*state.speed;
-      rect.color = getRandomVector();
-      gl.clearColor(...selectRandom(shapeColors));
-      function getRandomVector() {
-        return [Math.random(), Math.random(), Math.random()];
-      }
-    }
+
     
     function getRenderingContext() {
       var canvas = document.querySelector("canvas");
       canvas.width = canvas.clientWidth;
       canvas.height = canvas.clientHeight;
-      var gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+      var gl = canvas.getContext("webgl2") || canvas.getContext("experimental-webgl");
       if (!gl) {
         alert("Failed to get WebGL context. Your browser or device may not support WebGL.")
         return null;
       }
-      gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
+      gl.viewport(0, 0, canvas.width, canvas.height);
       gl.clearColor(0.0, 0.0, 0.0, 1.0);
       gl.clear(gl.COLOR_BUFFER_BIT);
       return gl;
